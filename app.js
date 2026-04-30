100b4b3ccf39ef18b506708d1b5f405cb611a757
peter-dorrell <peter.dorrell@proton.me>
2026-04-30 22:26:22 +0200

    ver1

--- app.js
+++
@@ -1,8 +0,0 @@
-// Main application logic
-console.log("Merge-BIM app initialized");
-
-// Example function to demonstrate structure
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM fully loaded and parsed");
-    // Additional logic will be added here
-});
